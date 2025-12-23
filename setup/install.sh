620fdffe3bac5de9dcf84cc972374bfefb3891c7
marlonzl7 <marlondesouzasampaio@gmail.com>
2025-12-23 04:17:52 -0300

    chore: removido script para instalação do flatpak e aplicações via flathub

--- setup/install.sh
+++ setup/install.sh
@@ -12,7 +12,5 @@ bash "$SCRIPT_DIR/base.sh"
 bash "$SCRIPT_DIR/clis.sh"
 bash "$SCRIPT_DIR/java.sh"
 bash "$SCRIPT_DIR/databases.sh"
-bash "$SCRIPT_DIR/desktop.sh"
 
 echo "Setup finalizado com sucesso!"
-echo "Reinicie a sessão (logout/login) para o Flatpak funcionar corretamente."
